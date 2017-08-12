5248b65fcb3f7c2966a72edbdc99de73d0aadef3
vivekbharatha <vivek.bharatha@gmail.com>
2017-08-12 11:47:54 +0530

    Basic react app init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return <h2>This is Vivek</h2>
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
