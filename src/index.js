055d260c3b5bedcfba0e2c8b7d0b835f52b9d3ca
vivekbharatha <vivek.bharatha@gmail.com>
2017-08-13 16:27:42 +0530

    Youtube search API added with independent components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/searchBar';
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+import VideList from './components/videoList';
+
+const APIKey = 'AIzaSyCO2KrVgWCZoyIbBbZiULeUoQlaBh6_5SQ';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+
+    YTSearch({ key: APIKey, term: 'gamerena' }, (videos) => {
+      this.setState({ videos });
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideList videos={ this.state.videos }/>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
