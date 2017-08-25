100cd18cbc2e5e0c5394c26c655f88e4970b2a18
vivekbharatha <vivek.bharatha@gmail.com>
2017-08-25 23:52:16 +0530

    Container definition

--- Redux-BookList/src/containers/bookList.js
+++ Redux-BookList/src/containers/bookList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
 
   renderList() {
     return this.props.books.map((book) => {
@@ -18,3 +19,11 @@ export default class BookList extends Component {
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
