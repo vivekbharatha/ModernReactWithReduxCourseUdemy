71d12298647e8b0662ace2c09eff98eb53cc3b6f
vivekbharatha <vivek.bharatha@gmail.com>
2017-08-28 20:12:17 +0530

    Search form event binding

--- Redux-Weather/src/containers/searchBar.js
+++ Redux-Weather/src/containers/searchBar.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { term: '' };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
   render() {
     return (
-      <form className="input-group">
-        <input className="form-control" />
+      <form className="input-group" onSubmit={this.onSearchFormSubmit}>
+        <input className="form-control"
+        placeholder="Grab 5 day forecast in any city"
+        value={this.state.term}
+        onChange={this.onInputChange}
+      />
         <span className="input-group-btn">
-          <button type="button" className="btn btn-secondary">Submit</button>
+          <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
       </form>
     );
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  onSearchFormSubmit(event) {
+    event.preventDefault();
+    // Let's call OpenWeatherAPI here and nail it!
   }
 }
