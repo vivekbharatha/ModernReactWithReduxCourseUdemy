99d4872b5e1eb98dcb5a66b985c845c5c51bf9af
vivekbharatha <vivek.bharatha@gmail.com>
2017-08-31 21:26:00 +0530

    Added styles and integration of google maps and refs intro

--- Redux-Weather/src/containers/weatherList.js
+++ Redux-Weather/src/containers/weatherList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Chart from './../components/chart';
+import GoogleMap from './../components/googleMap';
 
 class WeatherList extends Component {
   render() {
@@ -27,12 +28,13 @@ class WeatherList extends Component {
     const temps = cityObj.list.map(weather => weather.main.temp);
     const pressure = cityObj.list.map(weather => weather.main.pressure);
     const humidity = cityObj.list.map(weather => weather.main.humidity);
+    const { lat, lon } = cityObj.city.coord;
 
     return (
       <tr key={cityName}>
-        <td>{cityName}</td>
-        <td><Chart data={temps} color="red" units="K"/></td>
-        <td><Chart data={pressure} color="blue" units="hPa"/></td>
+        <td><GoogleMap lat={lat} lon={lon}/></td>
+        <td><Chart data={temps} color="blue" units="K"/></td>
+        <td><Chart data={pressure} color="green" units="hPa"/></td>
         <td><Chart data={humidity} color="black" units="%"/></td>
       </tr>
     )
