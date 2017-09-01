b3248b37ba744529b91ac1e0dec5a1283d699a95
vivekbharatha <vivek.bharatha@gmail.com>
2017-09-01 19:46:05 +0530

    Basics of Router

--- ReactRouter+ReduxForm-Blog/src/index.js
+++ ReactRouter+ReduxForm-Blog/src/index.js
@@ -2,14 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends React.Component {
+  render() { return <div>Wassup B!</div> };
+}
+
+class FOff extends React.Component {
+  render() { return <div>F Off!</div> };
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    {/* <App /> */}
+    <BrowserRouter>
+      <div>
+        <Route path="/wassup" component={Hello} />
+        <Route path="/f-off" component={FOff} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
