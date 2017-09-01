03d43210bad0eaca88943595b730368932e7b694
vivekbharatha <vivek.bharatha@gmail.com>
2017-09-01 19:57:14 +0530

    Root url posts component init

--- ReactRouter+ReduxForm-Blog/src/index.js
+++ ReactRouter+ReduxForm-Blog/src/index.js
@@ -4,26 +4,18 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
+// import App from './components/app';
+import Posts from './components/posts';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-  render() { return <div>Wassup B!</div> };
-}
-
-class FOff extends React.Component {
-  render() { return <div>F Off!</div> };
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     {/* <App /> */}
     <BrowserRouter>
       <div>
-        <Route path="/wassup" component={Hello} />
-        <Route path="/f-off" component={FOff} />
+        <Route path="/" component={Posts} />
       </div>
     </BrowserRouter>
   </Provider>
