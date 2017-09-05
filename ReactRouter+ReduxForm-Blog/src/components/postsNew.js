425331e2d9df797361c5fbf41c0a386fe809ca35
vivekbharatha <vivek.bharatha@gmail.com>
2017-09-05 15:04:12 +0530

    used redux-form to create form fields for post data

--- ReactRouter+ReduxForm-Blog/src/components/postsNew.js
+++ ReactRouter+ReduxForm-Blog/src/components/postsNew.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{ field.label }</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
-      <div>
-        Posts New
-      </div>
+      <form>
+        <Field
+          name="title"
+          label="Title"
+          component={this.renderField}
+        />
+        <Field
+          name="tags"
+          label="Tags"
+          component={this.renderField}
+        />
+        <Field
+          name="content"
+          label="Content"
+          component={this.renderField}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({ form: 'PostsNewForm' })(PostsNew);
